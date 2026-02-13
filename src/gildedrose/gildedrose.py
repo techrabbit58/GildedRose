5182ac0aad15c3b6805dbacf9beb447555fc64b8
techrabbit58 <dzb@pruefbit.de>
2026-02-13 18:56:36 +0100

    refactor: invert if-conditions, simplify increment/decrement operations, check "sulfuras" first

--- src/gildedrose/gildedrose.py
+++ src/gildedrose/gildedrose.py
@@ -3,36 +3,6 @@ from collections.abc import Iterable
 from gildedrose.goblincode import Item
 
 
-def update_one(item: Item) -> None:
-    if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-        if item.quality > 0:
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.quality = item.quality - 1
-    else:
-        if item.quality < 50:
-            item.quality = item.quality + 1
-            if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                if item.sell_in < 11:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-                if item.sell_in < 6:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-    if item.name != "Sulfuras, Hand of Ragnaros":
-        item.sell_in = item.sell_in - 1
-    if item.sell_in < 0:
-        if item.name != "Aged Brie":
-            if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                item.quality = item.quality - item.quality
-        else:
-            if item.quality < 50:
-                item.quality = item.quality + 1
-
-
 class GildedRose(object):
 
     def __init__(self, items: Iterable[Item]) -> None:
@@ -41,3 +11,34 @@ class GildedRose(object):
     def update_quality(self):
         for item in self.items:
             update_one(item)
+
+
+def update_one(item: Item) -> None:
+
+    if item.name == "Sulfuras, Hand of Ragnaros":
+        return
+
+    if item.name == "Aged Brie" or item.name == "Backstage passes to a TAFKAL80ETC concert":
+        if item.quality < 50:
+            item.quality += 1
+            if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                if item.sell_in <= 10 and item.quality < 50:
+                    item.quality += 1
+                if item.sell_in <= 5 and item.quality < 50:
+                    item.quality += 1
+    else:
+        if item.quality > 0:
+            item.quality -= 1
+
+    item.sell_in -= 1
+
+    if item.sell_in < 0:
+        if item.name == "Aged Brie":
+            if item.quality < 50:
+                item.quality += 1
+        else:
+            if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                item.quality = 0
+            else:
+                if item.quality > 0:
+                    item.quality -= 1
