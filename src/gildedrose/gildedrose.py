0bf6e450092777e2c2257457f85b725aa0108677
techrabbit58 <dzb@pruefbit.de>
2026-02-13 19:15:44 +0100

    refactor: decrement sell-in is last update action, simplify sub-zero update logic

--- src/gildedrose/gildedrose.py
+++ src/gildedrose/gildedrose.py
@@ -32,14 +32,11 @@ def update_one(item: Item) -> None:
 
     if item.sell_in <= 0:
         if item.name == "Aged Brie":
-            if item.quality < 50:
-                item.quality += 1
+            item.quality = min(50, item.quality + 1)
+        elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+            item.quality = 0
         else:
-            if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                item.quality = 0
-            else:
-                if item.quality > 0:
-                    item.quality -= 1
+            item.quality = max(0, item.quality - 1)
 
     item.sell_in -= 1
 
